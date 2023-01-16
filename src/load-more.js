2d2a7b25376cdcc513db1bf94885e93b69a6b856
MihajloGerbach <108993799+MihajloGerbach@users.noreply.github.com>
2023-01-16 19:51:18 +0300

    hw

--- src/load-more.js
+++ src/load-more.js
@@ -1,42 +1,21 @@
-export default function createGalleryCard(objCart, refs, btnLoadMore) {
-  if (!objCart.hits) {
-    refs.divGallery.insertAdjacentHTML(
-      'beforeend',
-      "<p class = 'info-massage'>We're sorry, but you've reached the end of search results.</p>"
-    );
-    btnLoadMore.hide();
-  } else {
-    return objCart.hits
-      .map(
-        ({
-          webformatURL,
-          largeImageURL,
-          tags,
-          likes,
-          views,
-          comments,
-          downloads,
-        }) => {
-          return `<div class="photo-card">
-      <a href="${largeImageURL}"><img width=340 height=226 src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
-  
-  <div class="info">
-    <p class="info-item">
-      <b>Likes ${likes}</b>
-    </p>
-    <p class="info-item">
-      <b>Views ${views}</b>
-    </p>
-    <p class="info-item">
-      <b>Comments ${comments}</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads ${downloads}</b>
-    </p>
-  </div>
-</div>`;
-        }
-      )
-      .join('');
+export default class BtnLoadMore {
+  constructor({ classBtn, hidden = false }) {
+    this.refs = this.getRefs(classBtn);
+    hidden && this.hide();
+  }
+
+  getRefs(classBtn) {
+    const refs = {
+      btnLoadMore: document.querySelector(`${classBtn}`),
+    };
+    return refs;
+  }
+
+  hide() {
+    this.refs.btnLoadMore.classList.add('is-hidden');
+  }
+
+  show() {
+    this.refs.btnLoadMore.classList.remove('is-hidden');
   }
 }
